3daac63468536055701d6942fb53ca3fd23047c1
ggermashev <ggermashev@gmail.com>
2025-05-19 19:42:23 +0300

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { AppProvider } from './app/index.ts'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <BrowserRouter basename='https://ggermashev.github.io/sf-frontend'>
     <AppProvider>
       <App />
     </AppProvider>
